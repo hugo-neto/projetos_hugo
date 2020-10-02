9be4f6dff8066a25e162922d168cb1901fb3a21b
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2020-10-02 09:20:42 -0300

    Trab. 2

--- python/Faculdade/MetodosNumericos/Entrega02.py
+++ python/Faculdade/MetodosNumericos/Entrega02.py
@@ -1,6 +1,25 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, aux = np.linspace(0, 10, 500), -1
+
 y = [-0.4, 0.4, 0.5, 0.4]
 x = [-0.5, 0.5, 1, 2]
+x_user = [-1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 2.51]
+y_user = []
 p = []
+
+
+def limpa_interpoladores(p):
+    p.clear()
+
+
+def gera_y_user(x_user):
+    for i in range(0,len(x_user)):
+        aux = interpoladores_lagrange(x_user[i], x, y, p)
+        y_user.append(aux)
+    return y_user
 
 
 def interpoladores_lagrange(xe, x, y, p):
@@ -10,23 +29,58 @@ def interpoladores_lagrange(xe, x, y, p):
             if i != j:
                 p[i] = (p[i])*((xe - x[j])/(x[i] - x[j]))
 
-
-def calcula_funcao(xe, x, y, p):
     final = float(0)
     for k in range(0,len(x)):
         final = final + p[k]*y[k]
+    
+    limpa_interpoladores(p)
 
-    p = []
     return final
 
 
+def grafico(x, y, p):
+    aux = -1
+    fig, ax = plt.subplots()
+    
+    # xt -> x TOTAL
+    # ft -> f(xt)    
+    xt, ft = [], []    
+
+    for i in range(0,3001):
+        aux = aux + 0.001
+        xt.append(aux)
+        aux2 = interpoladores_lagrange(aux, x, y, p)        
+        ft.append(aux2)
+        
+    x1 = np.array(x)
+    y1 = np.array(y)
+    
+    x2 = np.array(xt)
+    fx = np.array(ft)
+
+    x3 = np.array(x_user)
+    y3 = np.array(y_user)  
+
+    primeiros_pontos, = ax.plot(x1, y1, label='Pontos Interpoladores')
+    funcao_lagrange, = ax.plot(x2, fx, label='f(x)')
+    funcao_lagrange.set_dashes([2, 2, 10, 2])
+    segundos_pontos, = ax.plot(x3, y3, label='Pontos Usuário')    
+    
+    ax.legend()    
+    plt.show()
+
+
 def main(xe, x, y, p):
-    interpoladores_lagrange(xe, x, y, p)
-    final = calcula_funcao(xe, x, y, p)
-    print(final)
-    print(p)
+    
+    for _ in range(0,4):
+        xe = xe + 0.1    
+        final = interpoladores_lagrange(xe, x, y, p)
+        print(final)
+    gera_y_user(x_user)
+    grafico(x, y, p)
 
 
 if __name__== '__main__':
-    xe = 1.2   
+    xe = 1.1   
     main(xe, x, y, p)
+
