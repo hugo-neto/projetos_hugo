be19db13d8f0088833b0c6a5e37d065adc9cc66e
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2020-10-19 16:25:28 -0300

    Update Entrega04.py

--- python/Faculdade/MetodosNumericos/Entrega04.py
+++ python/Faculdade/MetodosNumericos/Entrega04.py
@@ -1,11 +1,41 @@
 
 
-def main():
-    N = 3
-    A = []
+def gera_matriz(N):
+    a = []
     for i in range(0,N):
-        A.append( [0] * N ) 
-    print(A)
+        a.append( [0] * N ) 
+    return a
+
+
+def altera_matriz(a, N):
+    for i in range(0,N):
+        for j in range(0,N):
+            a[i][j] = float(input(f"Digite o termo a{i+1}{j+1}: "))
+    return a
+
+
+def printa(a, N):
+    for i in range(0, N):
+        print("")
+        for j in range(0, N):
+            print(a[i][j], end=" ")
+
+
+def Doolittle(a, N):
+    for k in range(0, N-1):
+        for i in range(k+1, N+1):
+            for j in range(k+1, N+1):
+                a[i][k] = a[i][k]/a[k][k]
+                a[i][j] = a[i][j] - a[i][k]*a[k][j]
+    return a
+
+
+def main():
+    aux, N = 0, 3
+    a = gera_matriz(N)
+    a = altera_matriz(a, N)
+    a = Doolittle(a, N)
+    printa(a, N)
 
 
 if __name__=='__main__':
