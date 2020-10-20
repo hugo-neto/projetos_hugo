ec7d2eeb4803868fed68b86c6bfcf9dcfa54a726
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2020-10-20 13:09:24 -0300

    Update Entrega04.py

--- python/Faculdade/MetodosNumericos/Entrega04.py
+++ python/Faculdade/MetodosNumericos/Entrega04.py
@@ -1,3 +1,18 @@
+# Falta: Vetor Pivotamento!
+# Falta: Pivotamento
+
+
+def gera_matriz_P(N):
+    P = []
+    for i in range(0, N):
+        P.append(i+1)
+    return P
+
+
+# N = int(input("Digite o valor de N da matriz quadrada NxN: "))
+N = 3
+# Variável Global
+P = gera_matriz_P(N)
 
 
 def gera_matriz(N):
@@ -14,6 +29,15 @@ def adiciona_valores_matriz(a, N):
     return a
 
 
+def gera_vetor_b(N):
+    b = []
+    print("Digite os valores do vetor b abaixo:")    
+    for i in range(0, N):
+        valor = float(input(f"Digite o valor do termo b[{i+1}]: "))
+        b.append(valor)
+    return b
+
+
 def printa(a, N):
     for i in range(0, N):
         print("")
@@ -22,8 +46,20 @@ def printa(a, N):
     print("")
 
 
-def pivota_matriz(a, N):
-    pass
+def pivota_matriz(a, N, i):
+    global P
+    comparativo = abs(a[i][i])
+    for j in range(i,N):
+        if abs(a[j][i]) > comparativo:
+            comparativo = a[j][i]
+            aux = a[j]
+            a[j] = a[i]
+            a[i] = aux
+            
+            aux = P[i]
+            P[i] = P[j]
+            P[j] = aux
+    return a
 
 
 def extrai_L(a, N):
@@ -43,9 +79,36 @@ def extrai_U(a, N):
     return U
 
 
+def troca_b(b, N):
+    for i in range(0, N):
+        if (P[i] - 1) != i:
+            p_indice = (P[i] - 1)
+            valor_b_depois = b[p_indice]
+            valor_b_antes  = b[i]
+            b[i] = valor_b_depois
+            b[p_indice] = valor_b_antes
+            P[i] = i+1
+            P[p_indice] = p_indice + 1
+    return b
+
+
+def subs_direta(b, N):
+    y = []
+    y.append(b[0])
+    for i in range(1,N):
+        aux = 
+    return y
+
+
+def printa_b(b, N):
+    for i in range(0, N):
+        print(b[i])
+
+
 def Doolittle(a, N):
     for k in range(0, N-1):
-        # a = pivota_matriz(a, N)
+        a = pivota_matriz(a, N, k)
+        # AQUI PRECISA DE UM PIVOTAMENTO
         for i in range(k+1, N):
             a[i][k] = a[i][k]/a[k][k]
         for i in range(k+1, N):
@@ -55,13 +118,16 @@ def Doolittle(a, N):
 
 
 def main():
-    aux, N = 0, 3
+    # Gera a matriz quadrada NxN e um vetor
+    # de pivotamento: [1, 2, 3, ... , N]
     a = gera_matriz(N)
     a = adiciona_valores_matriz(a, N)
+    b = gera_vetor_b(N)
     a = Doolittle(a, N)
     L = extrai_L(a, N)
     U = extrai_U(a, N)
-    printa(U, N)
+    b = troca_b(b, N)
+    printa(a, N)
 
 
 if __name__=='__main__':
