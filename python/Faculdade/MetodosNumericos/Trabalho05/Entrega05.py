4140aa5a1ca4d9204e95ea843ae30420f0a7f342
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2020-10-29 19:57:24 -0300

    Create Entrega05.py

---
+++ python/Faculdade/MetodosNumericos/Trabalho05/Entrega05.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    a = float(input("Digite o valor de a: "))
+    b = float(input("Digite o valor de b: "))
+    N = float(input("Digite o valor de N: "))
+    criterio_convergencia = float(input("Digite o valor do critério de convergência: "))
+    epslon = float(input("Digite o valor de epslon (Menor valor do passo de integração): "))    
+
+
+if __name__=='__main__':
+    main()
