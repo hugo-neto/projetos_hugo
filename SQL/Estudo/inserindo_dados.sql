85ad4d12a602efdb62dbc90f5e5ea94e5313b212
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-17 10:02:16 -0300

    Update inserindo_dados.sql

--- SQL/Estudo/inserindo_dados.sql
+++ SQL/Estudo/inserindo_dados.sql
@@ -1,1 +1,51 @@
-/*A*/
+/*
+Sintaxe básica de inserção:
+INSERT INTO tabela VALUES ('string1', 'string2', 123);
+
+Sintaxe especificando as colunas e ordem da inserção:
+INSERT INTO tabela(COLUNA1, COLUNA2, COLUNA3) VALUES ('string1', 'string2', 123);
+ou
+INSERT INTO tabela(COLUNA1, COLUNA2, COLUNA3) VALUES 
+('string1', 'string2', 123),
+('string3', 'string4', 456);
+NULL -> um valor nulo!
+*/
+
+/*
+Atente-se ao fato que int é até um valor fixo!
+Mesmo sendo int(20) ele não passa de um valor máximo
+*/
+
+CREATE DATABASE PROJETO;
+
+/* CONECTANDO-SE AO BANCO */
+
+USE PROJETO;
+
+/* CRIANDO A TABELA DE CLIENTES */
+
+CREATE TABLE CLIENTE(
+	NOME VARCHAR(30),
+	SEXO CHAR(1),
+	EMAIL VARCHAR(30),
+	CPF INT(11),
+	TELEFONE VARCHAR(30),
+	ENDERECO VARCHAR(100)
+
+);
+
+INSERT INTO CLIENTE VALUES('JOAO','M','JOAO@GMAIL.COM',988638273,'22923110','MAIA LACERDA - ESTACIO - RIO DE JANEIRO - RJ');
+
+INSERT INTO CLIENTE VALUES('CELIA','F','CELIA@GMAIL.COM',541521456,'25078869','RIACHUELO - CENTRO - RIO DE JANEIRO - RJ');
+
+INSERT INTO CLIENTE VALUES('JORGE','M',NULL,885755896,'58748895','OSCAR CURY - BOM RETIRO - PATOS DE MINAS - MG');
+
+INSERT INTO CLIENTE(NOME,SEXO,ENDERECO,TELEFONE,CPF) VALUES('LILIAN','F','SENADOR SOARES - TIJUCA - RIO DE JANEIRO - RJ','947785696',887774856);
+
+INSERT INTO CLIENTE VALUES('ANA','F','ANA@GLOBO.COM',85548962,'548556985','PRES ANTONIO CARLOS - CENTRO - SAO PAULO - SP'),
+                          ('CARLA','F','CARLA@TERATI.COM.BR',7745828,'66587458','SAMUEL SILVA - CENTRO - BELO HORIZONTE - M');
+                                               
+                          
+                          
+                          
+      
