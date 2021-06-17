a6806de3c986a5266dc12c5fb7f5cf1117910c8c
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-17 10:27:08 -0300

    Update projeção_dados.sql

--- SQL/Estudo/projeção_dados.sql
+++ SQL/Estudo/projeção_dados.sql
@@ -1,1 +1,16 @@
-/*A*/
+/* Projeção de colunas que não existem em CLIENTE*/
+SELECT NOW() as DATA_HORA, 'HUGO' as Professor;
+
+SELECT NOME, SEXO, EMAIL, ENDERECO FROM CLIENTE;
+
+/*Projeta a coluna NOME como CLIENTE
+usando sintaxe COLUNA AS NOME_DESEJADO*/
+SELECT NOME AS CLIENTE, SEXO, EMAIL FROM CLIENTE;
+
+/*SOMENTE PARA FINS ACADÊMICOS
+MOSTRA TUDO!!*/
+SELECT * FROM CLIENTE
+
+/*Projeta tabelas que existem e não existem no banco*/
+SELECT EMAIL, SEXO, ENDERECO, NOME, NOW() AS DATA_HORA FROM CLIENTE;
+
