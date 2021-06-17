bf5015ea989e84faac917459a9d7421f64098817
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-17 09:06:50 -0300

    Update projeto_iniciante.sql

--- SQL/Estudo/projeto_iniciante.sql
+++ SQL/Estudo/projeto_iniciante.sql
@@ -2,12 +2,15 @@
 https://drive.google.com/drive/folders/1dMd4WcS3TRD2GDpXGiSa9ZqgDPwxa9sl?usp=sharing
 */
 /* Seleciona o Banco de Dados treino */
-use treino;
+
+CREATE DATABASE treino;
+
+USE treino;
 
 /* Cria uma tabela clientes */
 
 CREATE TABLE CLIENTE (
-    NOME VARCHAR(45),
+	NOME VARCHAR(45),
     SEXO CHAR(1),
     EMAIL VARCHAR(45),
     CPF INT(11),
