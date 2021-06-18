cb1ec8e1fcfdf311a6d23c3a58dea3ac1ff0d3d3
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-18 16:14:30 -0300

    Create Exercicio_01.sql

---
+++ SQL/Estudo/Exercicios/Exercicio_01.sql
@@ -0,0 +1,26 @@
+/* Acesso aos documentos:
+https://drive.google.com/drive/folders/1dMd4WcS3TRD2GDpXGiSa9ZqgDPwxa9sl?usp=sharing
+Seleciona o Banco de Dados treino */
+
+/*Cria uma database treino*/
+CREATE DATABASE treino;
+
+/*Conecta o Banco de Dados*/
+USE treino;
+
+/* Cria uma tabela clientes */
+CREATE TABLE CLIENTE (
+	NOME VARCHAR(45),
+    SEXO CHAR(1),
+    EMAIL VARCHAR(45),
+    CPF INT(11),
+    TELEFONE VARCHAR(30)
+);
+
+/*Mostra todas tabelas*/
+SHOW TABLES;
+
+/*Mostra a descrição/campos da tabelas
+DESC = Describe
+*/
+DESC CLIENTE;
