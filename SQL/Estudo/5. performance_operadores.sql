902f517eb7bb125623ec0ade156b61cfa8273d5f
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-18 17:43:39 -0300

    Update 5. performance_operadores.sql

--- SQL/Estudo/5. performance_operadores.sql
+++ SQL/Estudo/5. performance_operadores.sql
@@ -9,6 +9,12 @@ SELECT COUNT(*) AS "Registros Treino" FROM treino.CLIENTE;
 
 /*Sem a ação group by ele entrega o número de registros
 da função count e a primeira linha da coluna SEXO*/
-SELECT SEXO, COUNT(*) 
+SELECT SEXO, COUNT(*) AS 'Quantidade'
 FROM LIVROS 
 GROUP BY SEXO;
+
+/*Lembrar que:
+Caso haja um banco de dados com 70% F e 30% RJ num condicional OR
+Colocar primeiro S = ‘F’ OR UF = ‘RJ’ para economizar performance.
+Para o AND a lógica é inversa
+*/
