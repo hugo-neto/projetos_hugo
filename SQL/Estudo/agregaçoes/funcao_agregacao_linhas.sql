76743bd53c69b3919a5727e2d924b33833af1ff9
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-06-30 09:44:08 -0300

    Create funcao_agregacao_linhas.sql

---
+++ SQL/Estudo/agregaçoes/funcao_agregacao_linhas.sql
@@ -0,0 +1,9 @@
+SELECT NOME,
+	   JANEIRO,
+	   FEVEREIRO,
+	   MARCO,
+	   (JANEIRO+FEVEREIRO+MARCO) AS "TOTAL",
+	   TRUNCATE((JANEIRO+FEVEREIRO+MARCO)/3,2) AS "MEDIA"
+	   FROM VENDEDORES;
+
+/*Pode-se adicionar outras funcionalidades também!*/
