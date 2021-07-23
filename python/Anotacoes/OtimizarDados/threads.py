343258a70909b3e8f7927920332318f0410efdab
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-07-23 17:55:06 -0300

    Update threads.py

--- python/Anotacoes/OtimizarDados/threads.py
+++ python/Anotacoes/OtimizarDados/threads.py
@@ -16,6 +16,9 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [7, 6, 5, 4, 3, 2, 1]
     results = executor.map(do_something, secs)
 
+ for result in results:
+    print(results)
+    
 b = time()
 
 do_something(7)
