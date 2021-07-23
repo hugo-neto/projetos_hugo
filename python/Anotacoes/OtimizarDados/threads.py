8350f85bbe540074713d73fdea5c2f1b7add0e86
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-07-23 13:31:37 -0300

    Create threads.py

---
+++ python/Anotacoes/OtimizarDados/threads.py
@@ -0,0 +1,31 @@
+
+import concurrent.futures
+from time import time
+
+
+def do_something(var):
+    x = 0
+    while x < 10000000:
+        x += 1
+    print(f"Terminou: {var}")
+
+a = time()
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [7, 6, 5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
+
+b = time()
+
+do_something(7)
+do_something(6)
+do_something(5)
+do_something(4)
+do_something(3)
+do_something(2)
+do_something(1)
+
+c = time()
+
+print(f"Primeiro: {b-a}")
+print(f"Segundo: {c-b}")
