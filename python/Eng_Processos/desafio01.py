39b77f64116ac669f6a0e4b565b49c80d1356a91
hugo-neto <65546638+hugo-neto@users.noreply.github.com>
2021-12-21 22:33:11 -0300

    Update desafio01.py

--- python/Eng_Processos/desafio01.py
+++ python/Eng_Processos/desafio01.py
@@ -1,3 +1,4 @@
+# https://www.delftstack.com/pt/howto/python-pandas/slice-columns-in-pandas-dataframe/#:~:text=3%200.377612%200.310408-,Utilize%20redindex()%20para%20fatiar%20colunas%20em%20Pandas%20DataFrame,para%20o%20corte%20de%20colunas.
 
 
 import pandas as pd
@@ -13,7 +14,8 @@ def coleta_excel(cond=False):
             #caminho = os.path.join(os.path.abspath('.'),(nome+extensao))
             #print(caminho)
             #df = pd.read_excel(str(caminho))
-            df = pd.read_excel(os.path.join(os.path.abspath('.'),"planilha.xls"))
+            #print(os.path.abspath('.'))
+            df = pd.read_excel(os.path.join(os.path.abspath('.'),"Dados.xls"))
             cond = True
         except:
             print("Problemas com o nome ou com a extensão!")
@@ -30,12 +32,24 @@ def calcula_dispersao(df):
     avalia_decisao(df, vetor)
 
 
+# Busca o valor de colatilidade
 def busca_volatilidade(df, coluna, indice):
     return df[coluna][indice]
 
 
-def busca_indice_maximo(vetor):
-    return vetor.index(max(vetor))
+# Corta o df de acordo com o interesse
+def corta_df(df, indice):
+    # Corta até o índice
+    vetor_f = []
+    df1 = df.iloc[0:(indice)+1, :]
+    df2 = df.iloc[indice:, :]
+    vetor_f.append(df1)
+    vetor_f.append(df2)
+    return vetor_f
+
+
+def abre_txt(nome="resultado_HugoNeto.txt"):
+    return open(nome, "w")
 
 
 # Avalia a decisão do corte
@@ -48,54 +62,93 @@ def avalia_decisao(df, vetor):
     aux.append(min(vetor[0], vetor[2]))
     aux.append(min(vetor[0], vetor[3]))
     
-    indice = busca_indice_maximo(aux)
-    
-    print(indice)
-    # print(vetor)
-    # print(aux)
-    # sys.exit()
+    # Busca índice referente ao valor máximo para aplicar regras herísticos
+    indice = aux.index(max(aux))
 
+    # Regra V1a
     if indice == 0:
-        linha = df.index[df[coluna_vazao] == df[coluna_vazao].max()]
-        linhaSuperior = linha-1 if linha != 0 else 0
-        # Ver qual das volatilidades é maior --> chama função busca_volatilidade
-        vol_maxima = ([busca_volatilidade(df, coluna_volatilidade, linha),
-        busca_volatilidade(df, coluna_volatilidade, linhaSuperior)
-        ])
-        # Fazer o corte
+        linha = list(df.index[df[coluna_vazao] == df[coluna_vazao].max()])[0]
 
+        if linha == 0:
+            return corta_df(df, linha)
+        
+        if busca_volatilidade(df, coluna_volatilidade, linha) > busca_volatilidade(df, coluna_volatilidade, linha-1):
+                # corte realizado no meio e resto
+            return corta_df(df, linha)
+        else:
+        # corte realizado meio-1
+            return corta_df(df, linha-1)
+
+    # Regra V1b
     elif indice == 1:
         n_linhas = len(df.index)
 
         # Ver se é par
         if n_linhas % 2 == 0:
             # Sem problemas, divide no meio
-            pass
+            linhas = len(df)/2
+            return corta_df(df, linhas)
         else:
             # Descobre menor relatividade
-            pass
-        
-
-        
+            # Pivo do meio da lista
+            cont = int(len(df)/2)
+            if busca_volatilidade(df, coluna_volatilidade, cont) > busca_volatilidade(df, coluna_volatilidade, cont-1):
+                # corte realizado no meio e resto
+                return corta_df(df, cont)
+            else:
+                # corte realizado meio-1
+                return corta_df(df, cont-1)
+    
+    # Regra V2
     elif indice == 2:
-        # Busca o índice referente maior volatilidade
-        linha = df.index[df[coluna_volatilidade] == df[coluna_volatilidade].max()]
+        # Busca o índice referente maior volatilidade (formato estranho) + converte em lista + int em seguida
+        linha = list(df.index[df[coluna_volatilidade] == df[coluna_volatilidade].max()])[0]
         # Realiza o corte
+        return corta_df(df, linha)
 
-    return aux
+
+def inicia_cortes(df):
+    vetor_final = calcula_dispersao(df)
+    
+    arquivo = abre_txt()
+    texto = '['
+    texto = texto + ''.join(str(componente) for componente in vetor_final[0].iloc[:,0])
+    texto = texto + '/' + ''.join(str(componente) for componente in vetor_final[1].iloc[:,0])
+    texto = texto + ']'
+    arquivo.write(texto)
+
+    # Ver questão de dois cortes por etapa!
+    for item in vetor_final:
+        if type(item) == list and len(item) > 2:
+            vetor = inicia_cortes(item)
+    pass
 
 
+# A função main já cria o txt na pasta
 def main():
     # 1. Le excel
     # python3 opicional1.py
     df = coleta_excel()
+
     # 2. Trata dados
-    calcula_dispersao(df)
+    # Cria txt
+    resultado = open("resultado_HugoNeto.txt", "a")
+    # Abre ele com permissões de escrita
+    resultado = abre_txt()
+    # Declara string que disserta sobre o problema
+    texto = """
+    Método heurístico, assistido por lógica nebulosa
+    Objetivo: sínteses de um sistema de separação
+    Apenas colunas de destilação
+    """
+    # escreve no arquivo
+    resultado.write(texto)
+    # Após escrever, fecha
+    resultado.close()
+
+    inicia_cortes(df)
     
-    # 3. Fornece fluxograma
-    pass
 
 
 if __name__ == '__main__':
     main()
-
